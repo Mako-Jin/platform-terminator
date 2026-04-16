e8790e6ff5cf08a909ef2af86ffb7767826e42f4
w13259720691 <846288535@qq.com>
2026-04-16 11:31:26 +0800

    创建一个登录认证的公共服务

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # platform-terminator
 platform-terminator
+
+创建子服务步骤
+pnpm-workspace.yaml添加服务
+
+pnpm create vite platform/platform-portal --template react
+
+|-- platform-terminator
+    |-- platform 平台级别的服务
+    |-- system 系统
+        |-- server 服务
--- platform/platform-auth/vite.config.ts
+++ platform/platform-auth/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +12,7 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3001,
+    port: 3000,
     cors: true,
     headers: {
       'Access-Control-Allow-Origin': '*',
